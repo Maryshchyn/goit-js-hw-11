93fe82389f9754088aed6af64c4d1922914a54d8
Maryshchyn <maryshchyn89@gmail.com>
2022-08-29 09:18:10 +0300

    еке

---
+++ src/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    form: document.querySelector('.search-form'),
+    input: document.querySelector('.search-form__input'),
+    gallery: document.querySelector('.gallery'),
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,110 @@
+import Notiflix from 'notiflix';
+import markup from './markup';
+import getRefs from './get-refs';
+import ImagesApiService from './images-service';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import articlesTempl from './js/markup';
-import NewsApiService from './js/nevs-servise';
+// Initializing references to DOM elements
+const refs = getRefs();
+// Total number of pages in backend response
+let totalPages = 0;
+// Boolean variable that signals that we have reached the end of the collection
+let overflow = false;
 
-const searchForm = document.querySelector('.search-form');
-const articlesContainer = document.querySelector('.js-articles-container');
-const loadMoreBtn = document.querySelector('.load-more');
+// Creating an instance of a class ImagesApiService
+const imagesApiService = new ImagesApiService();
 
-const newApiService = new NewsApiService();
+// Add eventListener to the form
+refs.form.addEventListener('submit', onSearch);
 
-searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', onLoadMore);
-loadMoreBtn.classList.add('hidden');
-loadMoreBtn.classList.remove('load-more')
+// Creating an instance of a class SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+lightbox.on('show.lightbox');
 
-
+// Function that run on the form submit
 function onSearch(e) {
-    e.preventDefault();
-    loadMoreBtn.classList.add('load-more');
-    loadMoreBtn.classList.remove('hidden')
-    newApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-    newApiService.resetPage();
-    newApiService.fethArticle().then(appendArticlesMarkup);
+  e.preventDefault();
+  let currentQuery = e.target.elements.searchQuery.value.trim();
+  if (imagesApiService.query === currentQuery) {
+    return;
+  }
+  clearOutput();
+  imagesApiService.query = currentQuery;
+  imagesApiService.resetPage();
+  if (!imagesApiService.query) {
+    return;
+  } else {
+    imagesApiService
+      .fetchImages()
+      .then(({ data }) => {
+        totalPages = Math.ceil(data.totalHits / imagesApiService.HITS_PER_PAGE);
+        validationData(data);
+        render(data);
+      })
+      .catch(error => console.log(error));
+  }
+}
 
+// Creating an object of the IntersectionObserver class that monitors the need to access the backend for a new piece of data
+let observer = new IntersectionObserver(([entry], observer) => {
+  if (entry.isIntersecting) {
+    observer.unobserve(entry.target);
+    if (!imagesApiService.isLoading && !overflow) {
+      onLoadMore();
+    }
+  }
+}, {});
 
-
-};
-
+// A function that calls the server when more images need to be loaded
 function onLoadMore() {
-    newApiService.fethArticle().then(appendArticlesMarkup);
-};
+  imagesApiService
+    .fetchImages()
+    .then(({ data }) => {
+      render(data);
+      if (imagesApiService.page > totalPages) {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+        overflow = true;
+      }
+    })
+    .catch(error => console.log(error));
+}
 
-function appendArticlesMarkup(hits) {
-    articlesContainer.insertAdjacentHTML('beforeend', hits.map(articlesTempl).join(''));
+// Output cleaning function
+function clearOutput() {
+  refs.gallery.innerHTML = '';
+}
+
+// Markup render function
+function render(data) {
+  refs.gallery.insertAdjacentHTML(
+    'beforeend',
+    data.hits.map(hit => markup(hit)).join('')
+  );
+
+  const lastCard = refs.gallery.lastElementChild;
+  if (lastCard) {
+    observer.observe(lastCard);
+  }
+
+  lightbox.refresh();
+}
+
+// Backend response validation
+function validationData(data) {
+  // If no images were found for your request
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  // If pictures were found according to your request
+  else {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
 }
