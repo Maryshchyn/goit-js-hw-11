13f824e2c8f96ed9d5df110fa781ff9b69a9d02d
Maryshchyn <maryshchyn89@gmail.com>
2022-08-29 12:27:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,110 +1,100 @@
-import Notiflix from 'notiflix';
-import markup from './markup';
-import getRefs from './get-refs';
-import ImagesApiService from './images-service';
+import { Notify } from 'notiflix';
+import { APIService } from './js/api-service';
+import { getCardMarkup } from './js/templates';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Initializing references to DOM elements
-const refs = getRefs();
-// Total number of pages in backend response
-let totalPages = 0;
-// Boolean variable that signals that we have reached the end of the collection
-let overflow = false;
+import 'modern-normalize';
+import '../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
 
-// Creating an instance of a class ImagesApiService
-const imagesApiService = new ImagesApiService();
+const api = new APIService();
+const ligthbox = new SimpleLightbox('.gallery a');
 
-// Add eventListener to the form
-refs.form.addEventListener('submit', onSearch);
+let totalHits = 0;
+let currentHits = 0;
 
-// Creating an instance of a class SimpleLightbox
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
-lightbox.on('show.lightbox');
+const showMessage = ({ type = 'info', message = '' }) => {
+  Notify[type](message);
+};
 
-// Function that run on the form submit
-function onSearch(e) {
-  e.preventDefault();
-  let currentQuery = e.target.elements.searchQuery.value.trim();
-  if (imagesApiService.query === currentQuery) {
-    return;
-  }
-  clearOutput();
-  imagesApiService.query = currentQuery;
-  imagesApiService.resetPage();
-  if (!imagesApiService.query) {
-    return;
-  } else {
-    imagesApiService
-      .fetchImages()
-      .then(({ data }) => {
-        totalPages = Math.ceil(data.totalHits / imagesApiService.HITS_PER_PAGE);
-        validationData(data);
-        render(data);
-      })
-      .catch(error => console.log(error));
-  }
-}
-
-// Creating an object of the IntersectionObserver class that monitors the need to access the backend for a new piece of data
-let observer = new IntersectionObserver(([entry], observer) => {
-  if (entry.isIntersecting) {
-    observer.unobserve(entry.target);
-    if (!imagesApiService.isLoading && !overflow) {
-      onLoadMore();
-    }
-  }
-}, {});
-
-// A function that calls the server when more images need to be loaded
-function onLoadMore() {
-  imagesApiService
-    .fetchImages()
-    .then(({ data }) => {
-      render(data);
-      if (imagesApiService.page > totalPages) {
-        Notiflix.Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-        overflow = true;
-      }
-    })
-    .catch(error => console.log(error));
-}
-
-// Output cleaning function
-function clearOutput() {
+const clear = () => {
   refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+  api.resetPage();
+  totalHits = 0;
+  currentHits = 0;
+};
+
+const renderCards = cards => {
+  const markup = cards.map(getCardMarkup).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+};
+
+const loadMore = async () => {
+  const { hits, totalHits } = await api.fetchImages();
+  renderCards(hits);
+  api.incrementPage();
+  return totalHits;
+};
+
+const onFormSubmit = async e => {
+  e.preventDefault();
+  clear();
+  api.query = e.currentTarget.elements.searchQuery.value;
+  if(api.query === '') {
+   return
 }
+  totalHits = await loadMore();
+  ligthbox.refresh();
+  
+  if (totalHits === 0) {
+    showMessage({
+      type: 'failure',
+      message:
+        'Sorry, there are no images matching your search query. Please try again.',
+    });
+    return;
+  }
+  showMessage({
+    type: 'success',
+    message: `Hooray! We found ${totalHits} hits`,
+  });
+  if (totalHits <= 40) {
+    showMessage({
+      message: "We're sorry, but you've reached the end of search results.",
+    });
+    return;
+  }
+  
+ 
+  refs.loadMoreBtn.classList.remove('is-hidden');
+  currentHits += api.step;
+};
 
-// Markup render function
-function render(data) {
-  refs.gallery.insertAdjacentHTML(
-    'beforeend',
-    data.hits.map(hit => markup(hit)).join('')
-  );
+const onButtonClick = async e => {
+  await loadMore();
+  ligthbox.refresh();
+  currentHits += api.step;
+  if (currentHits >= totalHits) {
+    showMessage({
+      message: "We're sorry, but you've reached the end of search results.",
+    });
 
-  const lastCard = refs.gallery.lastElementChild;
-  if (lastCard) {
-    observer.observe(lastCard);
+    refs.loadMoreBtn.classList.add('is-hidden');
   }
 
-  lightbox.refresh();
-}
+  const { height: cardHeight } =
+    refs.gallery.firstElementChild.getBoundingClientRect();
 
-// Backend response validation
-function validationData(data) {
-  // If no images were found for your request
-  if (data.hits.length === 0) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-  // If pictures were found according to your request
-  else {
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  }
-}
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onButtonClick);
---
+++ src/js/api-service.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const KEY = '29541275-d631332db7b3701d55f9f7fc0';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export class APIService {
+  constructor() {
+    this.page = 1;
+    this.query = '';
+    this.step = 40;
+  }
+  
+  async fetchImages() {
+    const { data } = await axios.get(
+      `/?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.step}&page=${this.page}`
+    );
+    return data;
+  }
+  
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
